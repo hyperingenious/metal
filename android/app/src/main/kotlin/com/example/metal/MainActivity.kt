dc4999adfb22f60eb700d9ce02333ac4955cd731
hyperingenious <skbmasale941@gmail.com>
2025-06-28 16:18:03 +0530

    Jay hind

---
+++ android/app/src/main/kotlin/com/example/metal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.metal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/main_app_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MainAppScreen extends StatefulWidget {
+  @override
+  _MainAppScreenState createState() => _MainAppScreenState();
+}
+
+class _MainAppScreenState extends State<MainAppScreen> {
+  int _index = 0;
+
+  final tabs = [
+    Center(child: Text('Home')),
+    Center(child: Text('Settings')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Main App')),
+      body: tabs[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
